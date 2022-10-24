238fdafe6286c49894707592aee659810006ddd2
louis alibert <louis.alib19@gmail.com>
2022-10-24 16:58:26 +0200

    using react hook forms, to find type of control, yup installed

--- Screens/SignIn.tsx
+++ Screens/SignIn.tsx
@@ -1,10 +1,70 @@
-import { View, Text } from 'react-native'
+import { Text, View, TextInput, Button, Alert, StyleSheet } from 'react-native'
 import React from 'react'
+import { useForm, Controller } from 'react-hook-form';
+
+type FormData = {
+  email: string;
+  password: string;
+};
+
 
 export default function SignIn() {
+  const { control, handleSubmit, formState: { errors } } = useForm<FormData>({
+    defaultValues: {
+      email: '',
+      password: ''
+    }
+  });
+  const onSubmit = (data: any) => console.log(data);
+
   return (
     <View>
-      <Text>SignIn</Text>
+      <Controller
+        control={control}
+        rules={{
+         required: true,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            style={styles.input}
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        email="email"
+      />
+      {errors.email && <Text>This is required.</Text>}
+
+      <Controller
+        control={control}
+        rules={{
+         maxLength: 100,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            style={styles.input}
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        password="password"
+      />
+
+      <Button title="Submit" onPress={handleSubmit(onSubmit)} />
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  input: {
+    borderRadius: 5,
+
+  }
+})
--- Screens/SignUp.tsx
+++ Screens/SignUp.tsx
@@ -1,5 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useForm } from 'react-hook-form';
+
 
 export default function SignUp() {
   return (
