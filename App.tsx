6cf2a510f986dd43afddb306149489616067b475
AndreMcqu <104978076+AndreMcqu@users.noreply.github.com>
2022-10-25 16:11:20 +0200

    working flatlist

--- App.tsx
+++ App.tsx
@@ -1,33 +1,35 @@
 import React, {type PropsWithChildren} from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import Home from './Screens/Home';
-import SignIn from './Screens/SignIn';
-import SignUp from './Screens/SignUp';
+// import {NavigationContainer} from '@react-navigation/native';
+// import {createNativeStackNavigator} from '@react-navigation/native-stack';
+// import Home from './Screens/Home';
+// import SignIn from './Screens/SignIn';
+// import SignUp from './Screens/SignUp';
+// import Schools from './Screens/Schools';
 import Schools from './Screens/Schools';
 
-const Stack = createNativeStackNavigator<RouteParams>();
+// const Stack = createNativeStackNavigator<RouteParams>();
 
-export type RouteParams = {
-  Home: undefined;
-  SignUp: undefined;
-  SignIn: undefined;
-  Schools: undefined;
-};
+// export type RouteParams = {
+//   Home: undefined;
+//   SignUp: undefined;
+//   SignIn: undefined;
+//   Schools: undefined;
+// };
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={() => ({
-          headerShown: false,
-        })}>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="SignIn" component={SignIn} />
-        <Stack.Screen name="SignUp" component={SignUp} />
-        <Stack.Screen name="Schools" component={Schools} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Schools/>
+    // <NavigationContainer>
+    //   <Stack.Navigator
+    //     screenOptions={() => ({
+    //       headerShown: false,
+    //     })}>
+    //     <Stack.Screen name="Home" component={Home} />
+    //     <Stack.Screen name="SignIn" component={SignIn} />
+    //     <Stack.Screen name="SignUp" component={SignUp} />
+    //     <Stack.Screen name="Schools" component={Schools} />
+    //   </Stack.Navigator>
+    // </NavigationContainer>
   );
 };
 
