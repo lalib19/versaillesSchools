5241b1c1b21837dd1c2a4acbedb068298f21c448
AndreMcqu <104978076+AndreMcqu@users.noreply.github.com>
2022-10-26 16:47:04 +0200

    working on filter function

--- App.tsx
+++ App.tsx
@@ -1,35 +1,34 @@
 import React, {type PropsWithChildren} from 'react';
-// import {NavigationContainer} from '@react-navigation/native';
-// import {createNativeStackNavigator} from '@react-navigation/native-stack';
-// import Home from './Screens/Home';
-// import SignIn from './Screens/SignIn';
-// import SignUp from './Screens/SignUp';
-// import Schools from './Screens/Schools';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from './Screens/Home';
+import SignIn from './Screens/SignIn';
+import SignUp from './Screens/SignUp';
 import Schools from './Screens/Schools';
 
-// const Stack = createNativeStackNavigator<RouteParams>();
 
-// export type RouteParams = {
-//   Home: undefined;
-//   SignUp: undefined;
-//   SignIn: undefined;
-//   Schools: undefined;
-// };
+const Stack = createNativeStackNavigator<RouteParams>();
+
+export type RouteParams = {
+  Home: undefined;
+  SignUp: undefined;
+  SignIn: undefined;
+  Schools: undefined;
+};
 
 const App = () => {
   return (
-    <Schools/>
-    // <NavigationContainer>
-    //   <Stack.Navigator
-    //     screenOptions={() => ({
-    //       headerShown: false,
-    //     })}>
-    //     <Stack.Screen name="Home" component={Home} />
-    //     <Stack.Screen name="SignIn" component={SignIn} />
-    //     <Stack.Screen name="SignUp" component={SignUp} />
-    //     <Stack.Screen name="Schools" component={Schools} />
-    //   </Stack.Navigator>
-    // </NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={() => ({
+          headerShown: false,
+        })}>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="SignIn" component={SignIn} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+        <Stack.Screen name="Schools" component={Schools} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
--- Screens/Schools.tsx
+++ Screens/Schools.tsx
@@ -1,23 +1,35 @@
-import React, {useEffect, useState} from 'react';
-import {ActivityIndicator, FlatList, Text, View} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, FlatList, Text, View } from 'react-native';
 import useApiData from '../api/useApiData';
 import Card from '../Components/Card';
 
 const Schools = () => {
-  const {data, isLoaded} = useApiData(
+  //get All information from API
+  const { data, isLoaded } = useApiData(
     'https://data.education.gouv.fr/api/v2/catalog/datasets/fr-en-annuaire-education/records?where=code_postal%3D%2278000%22&limit=100&offset=0',
   );
 
   if (!isLoaded) {
     return <ActivityIndicator />;
   }
+  //filter data and return seclected data
+  // const [selected, setSelected] = React.useState(filters[0]);
+  // const [filters, setFilters] = React.useState([
+  //   { label: '' },
+  //   { label: 'A' },
+  //   { label: 'B' },
+  //   { label: 'C' },
+  // ]);
+
+  const schoolfilter = data.filter(isfiltered => isfiltered.record.fields.type_etablissement === "Ecole" && "Lycée");
+  console.log("school name" + schoolfilter);
 
   return (
-    <View style={{flex: 1, padding: 5}}>
+    <View style={{ flex: 1, padding: 5 }}>
       <FlatList
-        data={data}
+        data={schoolfilter}
         keyExtractor={item => item.id}
-        renderItem={({item}) => (
+        renderItem={({ item }) => (
           <Card
             nom_etabli={item.record.fields.nom_etablissement}
             type_etabli={item.record.fields.type_etablissement}
